0be95c1b4f57502043cbecf33bb3c4ed644afdae
Hadilou1998 <hadilissilame@gmail.com>
2025-08-13 00:32:34 +0200

    Updated PropertyCard

--- app/View/Components/PropertyCard.php
+++ app/View/Components/PropertyCard.php
@@ -2,18 +2,29 @@
 
 namespace App\View\Components;
 
+use App\Models\Property;
 use Closure;
 use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
 
 class PropertyCard extends Component
 {
+    /** 
+     * The property to display in the card.
+     * 
+     * @var \App\Models\Property
+
+     */
+    public Property $property;
+
     /**
      * Create a new component instance.
+     * 
+     * @param \App\Models\Property
      */
-    public function __construct()
+    public function __construct(Property $property)
     {
-        //
+        $this->property = $property;
     }
 
     /**
